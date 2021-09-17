548aaf0b0c1a26395445a5989d2b5ff131c976e9
ITALO PIOVAN <italo_piovan@hotmail.com>
2021-09-17 17:45:13 -0300

    Menu and Constants used in program

---
+++ 8 Puzzle/Application/App.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _8_Puzzle.Algorithms;
+
+namespace _8_Puzzle.Application
+{
+    class App
+    {
+        public void menu(int[,] initial, int[,] goal, int op)
+        {
+            switch(op)
+            {
+                case 1:
+                    AStar astar = new AStar();
+                    astar.solve(initial, goal);
+                    break;
+                case 2:
+                    //OutroAlgoritmo(initial, goal);
+                    break;
+            }
+        }
+    }
+}
